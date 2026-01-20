9bf260f74c53e20cf0f81959e00db9038c920504
ptuemmler <paul.tuemmler@uni-rostock.de>
2026-01-20 20:57:37 +0100

    Cleaned up injecting JavaScript example.

--- docs/apps/TemplatePlotly/animated_js/app.py
+++ docs/apps/TemplatePlotly/animated_js/app.py
@@ -1,4 +1,3 @@
-import numpy as np
 import plotly.graph_objects as go
 from shiny import App, Inputs, Outputs, Session, render, ui
 
@@ -14,9 +13,29 @@ app_ui = ui.page_sidebar(
 )
 
 
+def build_plot_from_js(javascript_text, **kwargs):
+    fig = go.Figure(
+    )
+
+    plot_html = fig.to_html(
+        auto_play=False,
+        include_plotlyjs=True,
+        full_html=False,
+        div_id="animated_plot"
+    )
+
+    for k, v in kwargs.items():
+        javascript_text = javascript_text.replace(k, str(v))
+
+    loop_script = f"""
+    <script>
+    {javascript_text}
+    </script>
+    """
+
+    return ui.HTML(plot_html + loop_script)
 
 def server(input: Inputs, output: Outputs, session: Session):
-
     @render.ui
     def plot():
         s = input.sigma()
@@ -26,91 +45,79 @@ def server(input: Inputs, output: Outputs, session: Session):
 
         n = input.n()
 
-        fig = go.Figure(
-        )
-
-        plot_html = fig.to_html(
-            auto_play=False,
-            include_plotlyjs=True,
-            full_html=False,
-            div_id="animated_plot"
-        )
-
-        loop_script = f"""
-        <script>
-        (function () {{
-          // ----- DESTROY PREVIOUS INSTANCE -----
-          if (window.lorenzAnimation) {{
-            cancelAnimationFrame(window.lorenzAnimation.frameId);
-            Plotly.purge('animated_plot');
-            window.lorenzAnimation = null;
-          }}
-
-          // ----- CREATE NEW INSTANCE -----
-          var controller = {{
-            frameId: null
-          }};
-          window.lorenzAnimation = controller;
-
-          var n = {n};
-          var x = [], y = [], z = [];
-          var dt = {dt};
-
-          var s = {s};
-          var b = {b};
-          var r = {r};
-
-          for (var i = 0; i < n; i++) {{
-            x[i] = Math.random() * 2 - 1;
-            y[i] = Math.random() * 2 - 1;
-            z[i] = 30 + Math.random() * 10;
-          }}
-
-         Plotly.react('animated_plot', [{{
-            x: x,
-            y: z,
-            mode: 'markers',
-          }}], {{
-            xaxis: {{
-              range: [-40, 40],
-            }},
-            yaxis: {{
-              range: [0, 60],
-            }}
-          }});
-
-
-          function compute () {{
-            for (var i = 0; i < n; i++) {{
-              var dx = s * (y[i] - x[i]);
-              var dy = x[i] * (r - z[i]) - y[i];
-              var dz = x[i] * y[i] - b * z[i];
-
-              x[i] += dx * dt;
-              y[i] += dy * dt;
-              z[i] += dz * dt;
-            }}
-          }}
-
-          function update () {{
-            compute();
-
-            Plotly.animate('animated_plot', {{
-              data: [{{ x: x, y: z }}]
-            }}, {{
-              transition: {{ duration: 0 }},
-              frame: {{ duration: 0, redraw: false }}
-            }});
-
+        loop_script="""
+        (function () {
+            // ----- DESTROY PREVIOUS INSTANCE -----
+            if (window.lorenzAnimation) {
+                cancelAnimationFrame(window.lorenzAnimation.frameId);
+                Plotly.purge('animated_plot');
+                window.lorenzAnimation = null;
+            }
+        
+            // ----- CREATE NEW INSTANCE -----
+            var controller = {
+                frameId: null
+            };
+            window.lorenzAnimation = controller;
+        
+            var n = VAR_n;
+            var x = [], y = [], z = [];
+            var dt = VAR_dt;
+        
+            var s = VAR_s;
+            var b = VAR_b;
+            var r = VAR_r;
+        
+            for (var i = 0; i < n; i++) {
+                x[i] = Math.random() * 2 - 1;
+                y[i] = Math.random() * 2 - 1;
+                z[i] = 30 + Math.random() * 10;
+            }
+        
+            Plotly.react('animated_plot', [{
+                x: x,
+                y: z,
+                mode: 'markers',
+            }], {
+                xaxis: {
+                    range: [-40, 40],
+                },
+                yaxis: {
+                    range: [0, 60],
+                }
+            });
+        
+        
+            function compute () {
+                for (var i = 0; i < n; i++) {
+                    var dx = s * (y[i] - x[i]);
+                    var dy = x[i] * (r - z[i]) - y[i];
+                    var dz = x[i] * y[i] - b * z[i];
+        
+                    x[i] += dx * dt;
+                    y[i] += dy * dt;
+                    z[i] += dz * dt;
+                }
+            }
+        
+            function update () {
+                compute();
+        
+                Plotly.animate('animated_plot', {
+                    data: [{ x: x, y: z }]
+                }, {
+                    transition: { duration: 0 },
+                    frame: { duration: 0, redraw: false }
+                });
+        
+                controller.frameId = requestAnimationFrame(update);
+            }
+        
             controller.frameId = requestAnimationFrame(update);
-          }}
-
-          controller.frameId = requestAnimationFrame(update);
-        }})();
-        </script>
+        })();
         """
 
-        return ui.HTML(plot_html + loop_script)
+        return build_plot_from_js(loop_script, VAR_n=n, VAR_dt=dt, VAR_s=s, VAR_b=b, VAR_r=r)
 
 
 app = App(app_ui, server)
